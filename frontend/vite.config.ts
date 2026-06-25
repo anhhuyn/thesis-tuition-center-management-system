454a39caf810d7d6482b01ccf1b643b30f784a56
anhhuyn <nguyenthianhhuyen03@gmail.com>
2026-06-25 11:42:43 +0700

    add

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+*/
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',  // Mặc định là dist
+  },
+  server: {
+    port: 5173,
+  },
+});
